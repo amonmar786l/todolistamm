23c31187d54cf332c67e102511fd01dab677f50b
amonmar786l <amonmar786l@iesmartinezm.es>
2022-01-26 10:50:19 +0100

    todolistamm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  todo = '';
+  todos: any[] = [];
+
+  addTodo() {
+    if (this.todo != '') {
+      this.todos = [...this.todos, this.todo];
+      this.todos + this.todo;
+      this.todo = "";
+    }
+  }
+
+  eliminar(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
